292ea791976116c2352c679a09de11fffc8f63dc
Senticat <romhojuit@yandex.ru>
2024-06-25 17:07:48 +0300

    v1.0

--- solar_model.py
+++ solar_model.py
@@ -19,8 +19,9 @@ def calculate_force(body, space_objects):
         if body == obj:
             continue  # тело не действует гравитационной силой на само себя!
         r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
-        body.Fx += 1  # FIXME: нужно вывести формулу...
-        body.Fy += 2  # FIXME: нужно вывести формулу...
+        F = (gravitational_constant * body.m * obj.m) / r**2
+        body.Fx += F * ((obj.x - body.x) / r)
+        body.Fy += F * ((obj.y - body.y) / r)
 
 
 def move_space_object(body, dt):
@@ -32,10 +33,12 @@ def move_space_object(body, dt):
     """
 
     ax = body.Fx/body.m
-    body.x += 42  # FIXME: не понимаю как менять...
-    body.Vx += ax*dt
-    # FIXME: not done recalculation of y coordinate!
+    body.Vx += ax * dt
+    body.x += body.Vx * dt
 
+    ay = body.Fy/body.m
+    body.Vy += ay * dt
+    body.y += body.Vy * dt
 
 def recalculate_space_objects_positions(space_objects, dt):
     """Пересчитывает координаты объектов.
--- solar_vis.py
+++ solar_vis.py
@@ -54,7 +54,7 @@ def scale_y(y):
     **y** — y-координата модели.
     """
 
-    return y  # FIXME: not done yet
+    return int(y*scale_factor) + window_height//2
 
 
 def create_star_image(space, star):
@@ -80,7 +80,11 @@ def create_planet_image(space, planet):
     **space** — холст для рисования.
     **planet** — объект планеты.
     """
-    pass  # FIXME: сделать как у звезды
+
+    x = scale_x(planet.x)
+    y = scale_y(planet.y)
+    r = planet.R
+    planet.image = space.create_oval([x - r, y - r], [x + r, y + r], fill=planet.color)
 
 
 def update_system_name(space, system_name):
@@ -108,7 +112,7 @@ def update_object_position(space, body):
     r = body.R
     if x + r < 0 or x - r > window_width or y + r < 0 or y - r > window_height:
         space.coords(body.image, window_width + r, window_height + r,
-                     window_width + 2*r, window_height + 2*r)  # положить за пределы окна
+                     window_width + 2*r, window_height + 2*r)
     space.coords(body.image, x - r, y - r, x + r, y + r)
 
 
